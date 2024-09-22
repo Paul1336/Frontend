e3aff8b7a7e1320b976a8efea92611e2c79549f7
CoKayne <duankayne@gmail.com>
2024-09-22 21:30:03 +0800

    style: colored and enlarged text for statistics page

--- src/app/Statistics/LargeWidget.tsx
+++ src/app/Statistics/LargeWidget.tsx
@@ -16,7 +16,7 @@ export type LargeWidgetType = {
 const LargeWidget: FunctionComponent<LargeWidgetType> = ({ className = "", description, usage, metric}) => {
   return (
     <div
-      className={`flex flex-col items-center justify-center self-stretch h-64 drop-shadow-[0px_1px_10px_rgba(0,_0,_0,_0.25)] max-w-full text-sec-black font-jost ${className}`}
+      className={`h-fit pt-[30px] pb-[30px] flex flex-col items-center justify-center self-stretch drop-shadow-[0px_1px_10px_rgba(0,_0,_0,_0.25)] max-w-full text-sec-black font-jost ${className}`}
     >
 
         <div className="w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] rounded-xl bg-bg-white"></div>
@@ -26,21 +26,20 @@ const LargeWidget: FunctionComponent<LargeWidgetType> = ({ className = "", descr
                 本周你已使用
             </h2>
             <h2 className="text-xl tracking-[-0.43px] font-[inherit] z-[2] self-end">
-                7
+                <span className="text-[3em] font-bold mx-[3px]">7</span>張酷胖
             </h2>
             <h2 className="text-xl tracking-[-0.43px] font-[inherit] z-[2]">
                 還差
             </h2>
-            <h2 className="text-xl tracking-[-0.43px] font-[inherit] z-[2] self-end">
-                3 張酷胖
+            <h2 className="text-xl tracking-[-0.43px] font-[inherit] z-[2] self-end text-act-yellow">
+                <span className="text-[3em] font-bold mx-[3px]">3</span> 張酷胖
             </h2>
             <h2 className="text-xl tracking-[-0.43px] font-[inherit] z-[2]">
-                就能獲得額外優惠
+                就能獲得<span className="text-act-yellow">額外優惠</span>
             </h2>
           </div>
 
         </div>
-
   );
 };
 
--- src/app/Statistics/SmallWidget.tsx
+++ src/app/Statistics/SmallWidget.tsx
@@ -16,25 +16,18 @@ export type SmallWidgetType = {
 const SmallWidget: FunctionComponent<SmallWidgetType> = ({ className = "", description, usage, metric}) => {
   return (
     <div
-      className={`flex flex-col items-center justify-center self-stretch h-40 drop-shadow-[0px_1px_10px_rgba(0,_0,_0,_0.25)] max-w-full text-sec-black font-jost ${className}`}
+      className={`h-fit py-[30px] flex flex-col items-center justify-center self-stretch h-40 drop-shadow-[0px_1px_10px_rgba(0,_0,_0,_0.25)] max-w-full text-sec-black font-jost ${className}`}
     >
 
         <div className="w-full absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] rounded-xl bg-bg-white"></div>
 
-          <div className="flex flex-col items-center justify-center gap-2">
+          <div className="flex flex-col items-center justify-center gap-5">
             <h2 className="text-xl tracking-[-0.43px] leading-[22px] font-[inherit] z-[2] inline-block">
                 {description}
             </h2>
-            <div className="flex flex-row gap-3 justify-end">
-              <div className="flex flex-row items-end gap-3">
-                <h1 className="text-[3em] tracking-[-0.43px] font-[inherit] z-[2] font-bold">
-                  {usage}
-                </h1>
                 <h2 className="text-xl tracking-[-0.43px] leading-[3em] font-[inherit] z-[2]">
-                  {metric}
+                  <span className="text-[3em] font-bold mx-[10px]">{usage}</span>{metric}
                 </h2>
-              </div>
-            </div>
           </div>
 
         </div>
